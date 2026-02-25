773a11aa8d59a6e2ffb63e9f3c525f6b436cb181
vishalthombare <vishalthombare862586@gmail.com>
2026-02-25 11:30:11 +0530

    Added Function js file

---
+++ Function.js
@@ -0,0 +1,14 @@
+console.log(Math.max(1, 2, 3, 6, 4, 4, 2, 5));
+
+/* Browser Function */
+
+alert("Good Morning");
+ confirm("Are you above 18");
+
+ const ans= confirm("Are you above 18");
+  if(ans){
+    console.log("Eligible to votting");
+  }
+  else{
+    console.log("Not eligible");
+  }

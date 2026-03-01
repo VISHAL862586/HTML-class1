20b6d9e4d6d29f6ba2a2269fed769bd4b8e3aafd
vishalthombare <vishalthombare862586@gmail.com>
2026-03-01 09:16:02 +0530

    Added InternalCall js file

---
+++ InternalCall.js
@@ -0,0 +1,23 @@
+
+/*function primeNo(){
+    let num = prompt("enter a Number");
+    let cnt = 0;
+    for(let i=1; i<=num; i++){
+        if(num%i==0){
+            cnt++;
+        }
+    }
+    if(cnt==2){
+        alert("Prime No");
+    }
+    else{
+        alert("Not Prime No.");
+    }
+} */
+
+function SumNo(){
+    let num1 = prompt("enter a Number");
+    let num2 = prompt("enter a Number");
+    let sum= parseInt(num1)+parseInt(num2);
+    alert(sum);
+}

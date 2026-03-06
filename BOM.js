2703e928c71490bc73a1a2404c706b12194bef4a
vishalthombare <vishalthombare862586@gmail.com>
2026-03-06 17:52:18 +0530

    Added js file

---
+++ BOM.js
@@ -0,0 +1,19 @@
+/*const btn = document.querySelector("button");
+const spanText = documnet.querySelector(".name");
+const name1 = document.querySelector("input");
+
+
+btn.addEventListener("click", ()=>{
+    spanText.innerText = name1.value;
+});
+*/
+
+
+
+const btn = document.querySelector("button");
+const spanText = document.querySelector(".name");
+const name1 = document.querySelector("input");
+
+btn.addEventListener("click", () => {
+    spanText.innerText = name1.value;
+});

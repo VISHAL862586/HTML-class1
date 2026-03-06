496b3157e2b551747084afbb32bf4dc39df8f863
vishalthombare <vishalthombare862586@gmail.com>
2026-03-06 18:18:53 +0530

    Upade code of js file

--- BOM1.js
+++ BOM1.js
@@ -1,7 +1,20 @@
-const btn = document.querySelector("button");
+/*const btn = document.querySelector("button");
 const inputText = document.querySelector("input");
 const h1El = document.createElement("h1");
 
 btn.addEventListener("click", ()=>{
     h1E1.appendChild(inputText.value);
-})
+})*/
+
+const btn = document.querySelector("button");
+const inputText = document.querySelector("input");
+const div = document.querySelector(".inputMsg");
+
+btn.addEventListener("click", () => {
+    const h1El = document.createElement("h1"); 
+    h1El.textContent = inputText.value; 
+    div.appendChild(h1El);
+
+
+   inputText.value = " ";
+});

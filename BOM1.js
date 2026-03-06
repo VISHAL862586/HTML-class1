784d05ef9aeb17ce39fb90da1065e90d1120bf2c
vishalthombare <vishalthombare862586@gmail.com>
2026-03-06 17:51:43 +0530

    Added js file

---
+++ BOM1.js
@@ -0,0 +1,7 @@
+const btn = document.querySelector("button");
+const inputText = document.querySelector("input");
+const h1El = document.createElement("h1");
+
+btn.addEventListener("click", ()=>{
+    h1E1.appendChild(inputText.value);
+})
